41308c2fb56e27a3f016ba45127283c231ddefa9
HUDuP <125272832+HUDuP@users.noreply.github.com>
2026-01-06 09:38:42 +0700

    Update map.js

--- map.js
+++ map.js
@@ -1,30 +1,22 @@
-<script>
-  var map;
-  var marker;
+var map;
+var marker;
 
-  function initMap() {
-    var location = { lat: 40.7128, lng: -74.0060 };
+function initMap() {
+  var location = { lat: 40.7128, lng: -74.0060 };
 
-    map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 12,
-      center: location
-    });
+  map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 12,
+    center: location
+  });
 
-    marker = new google.maps.Marker({
-      position: location,
-      map: map
-    });
-  }
+  marker = new google.maps.Marker({
+    position: location,
+    map: map
+  });
+}
 
-  // Function you can call from a button
-  function showMap(lat, lng) {
-    var pos = { lat: lat, lng: lng };
-    map.setCenter(pos);
-    marker.setPosition(pos);
-  }
-</script>
-
-<script
-  src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCrV3Y-C0wBMMqCx7Jx6Rx_Igo2e1Z4tj4&callback=initMap"
-  async defer>
-</script>
+function showMap(lat, lng) {
+  var pos = { lat: lat, lng: lng };
+  map.setCenter(pos);
+  marker.setPosition(pos);
+}
