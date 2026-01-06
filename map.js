243c0902ed600b8cca7e758dcbacf817b8a97a30
HUDuP <125272832+HUDuP@users.noreply.github.com>
2026-01-06 09:30:27 +0700

    Update map.js

--- map.js
+++ map.js
@@ -1,16 +1,30 @@
 <script>
+  var map;
+  var marker;
+
   function initMap() {
     var location = { lat: 40.7128, lng: -74.0060 };
 
-    var map = new google.maps.Map(document.getElementById("map"), {
+    map = new google.maps.Map(document.getElementById("map"), {
       zoom: 12,
       center: location
     });
 
-    var marker = new google.maps.Marker({
+    marker = new google.maps.Marker({
       position: location,
-      map: map,
-      title: "New York City"
+      map: map
     });
   }
+
+  // Function you can call from a button
+  function showMap(lat, lng) {
+    var pos = { lat: lat, lng: lng };
+    map.setCenter(pos);
+    marker.setPosition(pos);
+  }
+</script>
+
+<script
+  src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCrV3Y-C0wBMMqCx7Jx6Rx_Igo2e1Z4tj4&callback=initMap"
+  async defer>
 </script>
